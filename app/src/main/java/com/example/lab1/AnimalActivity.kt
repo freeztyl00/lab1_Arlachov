a3520dfd4721d74252ef4b9e77621e5e9e70d522
freeztyl00 <macios209@gmail.com>
2023-03-19 16:16:45 +0200

    final version

--- app/src/main/java/com/example/lab1/AnimalActivity.kt
+++ app/src/main/java/com/example/lab1/AnimalActivity.kt
@@ -4,7 +4,7 @@ package com.example.lab1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-
+import kotlinx.coroutines.Dispatchers.Main
 
 
 class AnimalActivity : AppCompatActivity() {
@@ -18,9 +18,11 @@ class AnimalActivity : AppCompatActivity() {
     {
         val name = intent.getStringExtra("name")
         val about = intent.getStringExtra("about")
-        val showName = findViewById<TextView>(R.id.textName)
-        showName.text = name
-        val showAbout = findViewById<TextView>(R.id.textAbout)
-        showAbout.text = about
+        val showName = findViewById<TextView>(R.id.textName).apply {
+            text = name
+        }
+        val showAbout = findViewById<TextView>(R.id.textAbout).apply {
+            text = about
+        }
     }
 }
--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -4,36 +4,51 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonCat = findViewById<Button>(R.id.button_cat)
+        buttonCat.setOnClickListener {
+            CatIntent()
+        }
+        val buttonMonkey = findViewById<Button>(R.id.button_monkey)
+        buttonMonkey.setOnClickListener {
+            MonkeyIntent()
+        }
+        val buttonHamster = findViewById<Button>(R.id.button_hamster)
+        buttonHamster.setOnClickListener {
+            HamsterIntent()
+        }
     }
 
-    val Cat = Animal("Абоба", "Я звичайна домашня кішка, яка полюбляє гратися.")
+    val Cat = Animal("Абоба", "Я домашня кішка, яка полюбляє гратися.")
     val Monkey = Animal("Давід", "Я іграшка у вигляді мавпочки.")
     val Hamster = Animal("Доктор Ватсон", "Я джунгарик, який бігає у колесі.")
-
-    fun CatIntent(view: View) {
-        val secActivity = Intent(this, AnimalActivity::class.java)
-        startActivity(secActivity)
-        intent.putExtra("name", Cat.name)
-        intent.putExtra("about", Cat.about)
+    fun CatIntent() {
+        val secActivity = Intent(this, AnimalActivity::class.java).also{
+            it.putExtra("name", Cat.name)
+            it.putExtra("about", Cat.about)
+            startActivity(it)
+        }
     }
 
-    fun MonkeyIntent(view: View) {
-        val secActivity = Intent(this, AnimalActivity::class.java)
-        startActivity(secActivity)
-        intent.putExtra("name", Monkey.name)
-        intent.putExtra("about", Monkey.about)
+    fun MonkeyIntent() {
+        val secActivity = Intent(this, AnimalActivity::class.java).also{
+            it.putExtra("name", Monkey.name)
+            it.putExtra("about", Monkey.about)
+            startActivity(it)
+        }
     }
 
-    fun HamsterIntent(view: View) {
-        val secActivity = Intent(this, AnimalActivity::class.java)
-        startActivity(secActivity)
-        intent.putExtra("name", Hamster.name)
-        intent.putExtra("about", Hamster.about)
+    fun HamsterIntent() {
+        val secActivity = Intent(this, AnimalActivity::class.java).also{
+            it.putExtra("name", Hamster.name)
+            it.putExtra("about", Hamster.about)
+            startActivity(it)
+        }
     }
 }
